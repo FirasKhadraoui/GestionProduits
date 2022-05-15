0a1bbe9a02af4d718ae975a460a01ce92689fee8
Firas Khadraoui <firas.khadraoui@esprit.tn>
2022-05-15 02:28:35 +0100

    Adding Controller using scaffold

---
+++ CRUDOperationASPDotNETMVCEFCore/CRUDOperationASPDotNETMVCEFCore/Models/Teacher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUDOperationASPDotNETMVCEFCore.Models
+{
+    public class Teacher
+    {
+        [Required]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Skills { get; set; }
+        [Range(5,50)]
+        public int TotalStudents { get; set; }
+        [Required]
+        public decimal Salary { get; set; }
+        [Required]
+        public DateTime AddedOn { get; set; }
+    }
+}
---
+++ CRUDOperationASPDotNETMVCEFCore/CRUDOperationASPDotNETMVCEFCore/Models/TeacherContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDOperationASPDotNETMVCEFCore.Models
+{
+    public class TeacherContext : DbContext
+    {
+        public TeacherContext(DbContextOptions<TeacherContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Teacher> Teachers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Teacher>(entity =>
+            {
+                entity.Property(e => e.Salary).IsRequired().HasColumnType("money");
+                entity.Property(e => e.Skills).IsRequired().HasMaxLength(250).IsUnicode(false);
+                entity.Property(e=> e.Name).IsRequired().HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.AddedOn).HasColumnType("date").HasDefaultValueSql("(getdate())");
+
+            }
+            );
+        }
+    }
+}
